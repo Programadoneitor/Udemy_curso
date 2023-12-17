225bb91506a0c58f698a0a4f413d5538b44e1113
Programadoneitor <138173834+Programadoneitor@users.noreply.github.com>
2023-12-17 11:57:19 -0500

    Create octavo_ejer.ts

---
+++ octavo_ejer.ts
@@ -0,0 +1,25 @@
+let carro = {
+  puertas: 3,
+  marca: 'suzuki',
+  color: 'azul marino',
+  anio: 2008,
+}
+
+let Tv = {
+  pulgadas: 85,
+  marca: 'LG',
+  anio: 2022,
+  tipo: 'Led'
+}
+
+let videosYoutube = {
+  videos: 10,
+  creador: 'Bad Bunny',
+  comentarios: ['Buena cancion','Like aqui para...','No puedo creer que...']
+  likes: '1M',
+  dislikes: 2500,
+}
+
+console.log (carro);
+console.log (Tv);
+console.log (videosYoutube);
