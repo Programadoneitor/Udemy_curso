78ffcc55666a214e86ac41bf1f55a28a303e5dcd
Programadoneitor <138173834+Programadoneitor@users.noreply.github.com>
2023-12-17 11:01:48 -0500

    Create sexto_ejer.ts

---
+++ sexto_ejer.ts
@@ -0,0 +1,7 @@
+
+let personas: string[] = ['Lenin','Sandoval','Eduardo'];
+let flores: string[] = ['Rosas','Girasoles','Lirios','Flor azul espinas rojas'];
+
+for ( let i = 0; i < flores.length; i++ ){
+  console.log( flores[i] );
+}
