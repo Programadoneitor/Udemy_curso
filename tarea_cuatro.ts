b029bb2165f8bd13d28ca1915e7d3664d2615777
Programadoneitor <138173834+Programadoneitor@users.noreply.github.com>
2023-12-17 11:11:44 -0500

    Create tarea_cuatro.ts

---
+++ tarea_cuatro.ts
@@ -0,0 +1,10 @@
+
+let personas: string[] = ['Lenin','Sandoval','Eduardo'];
+let salario: number[] = [450, 1200, 1000];
+
+for ( let i = 0; i < personas.length; i++ ){
+  let personas = personas[i];
+  let salario = salario[i];
+  
+  console.log( personas + ' tiene un salario de '+ salario);
+}
