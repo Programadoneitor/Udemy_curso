393ec304522159cedf3eab6d53d63da22ea4d5dd
Programadoneitor <138173834+Programadoneitor@users.noreply.github.com>
2023-12-17 01:06:58 -0500

    Create segundo_ejer.ts

---
+++ segundo_ejer.ts
@@ -0,0 +1,18 @@
+export let nombre: string = 'Lenin';
+let apellido: string  = ' Arevalo';
+
+let nombreCompleto: string = nombre + apellido;
+console.log(nombreCompleto);
+
+let number1: number = 10.5;
+let number2: number = 20.3;
+
+console.log( number1 + number2 );
+
+let isActive: boolean = true;
+let isRunning: boolean = false;
+let hasKids: boolean = true;
+
+console.log('isActive', isActive );
+console.log('isRunning', isRunning );
+console.log('hasKids', hasKids );
